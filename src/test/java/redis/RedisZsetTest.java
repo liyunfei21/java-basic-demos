7ab7e2e9721cbbc69506f303cb3b91c281121a27
yunfei <liyunfei@luvsec.com>
2019-12-09 16:14:34 +0800

    基础语法验证

--- src/test/java/redis/RedisZsetTest.java
+++ src/test/java/redis/RedisZsetTest.java
@@ -1,3 +1,5 @@
+package redis;
+
 import dto.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- src/test/java/suger/TryWith.java
+++ src/test/java/suger/TryWith.java
@@ -1,3 +1,5 @@
+package suger;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
